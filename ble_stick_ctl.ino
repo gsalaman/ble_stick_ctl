b2eb5779d6bfb97e3a7f03d590d37d07d78d19b9
Glenn Salaman <g.salaman@comcast.net>
2020-05-12 12:56:53 -0600

    Drop in cylon framework, ported to FastLED instead of Adafruit_NeoPixel

--- ble_stick_ctl.ino
+++ ble_stick_ctl.ino
@@ -1,4 +1,11 @@
-
+/* controlling the (tiny) stick with an ESP32 over BLE.
+ *  
+ * This rev imports the cylon-eye from cylon v2.
+ * Control is via these characteristics:  
+ *   - Speed
+ *   - Eye Color (passed as RGB)
+ *   - Background Color (passed as RGB).
+ */
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
@@ -11,9 +18,41 @@
 #define LED_TYPE WS2812
 #define COLOR_ORDER GRB
 
-CRGB leds[NUMPIXELS];
 
+#define MAX_EYE_SIZE (NUMPIXELS - 1)
 
+/* Window concepts:
+ *  The virtual window is a large array of pixels, but we're only going to 
+ *  show the middle 8 pixels...and we'll refer to those as the "real window" 
+ *  pixels. 
+ */
+#define VIRTUAL_WINDOW_SIZE (NUMPIXELS+2*MAX_EYE_SIZE)
+CRGB virtual_window[VIRTUAL_WINDOW_SIZE];
+
+#define REAL_WINDOW_START_INDEX MAX_EYE_SIZE 
+#define REAL_WINDOW_END_INDEX (REAL_WINDOW_START_INDEX + NUMPIXELS - 1)
+CRGB *real_window = &(virtual_window[REAL_WINDOW_START_INDEX]);
+CRGB *leds = real_window;
+
+/* The cyclon eye parameters */
+int eye_head_pos=REAL_WINDOW_START_INDEX;  // this is the virutal window index
+int eye_size=7;
+
+typedef enum
+{
+  DIR_RIGHT,
+  DIR_LEFT
+} dir_type;
+
+dir_type current_eye_dir=DIR_RIGHT;
+
+CRGB eye_color=CRGB::Red;
+CRGB background_color=CRGB::Blue;
+
+#define DISPLAY_MIN_DELAY 10
+#define DISPLAY_MAX_DELAY 1000
+
+uint32_t display_delay_ms=100;
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -96,6 +135,141 @@ class MyCallbacks2: public BLECharacteristicCallbacks
     }
 };
 
+/* Fills the entire virtual window with a given color */
+void virtual_fill(CRGB color)
+{
+    fill_solid(virtual_window, VIRTUAL_WINDOW_SIZE, color);
+}
+
+/*=================================================
+ * display_pixels
+ * 
+ * This function displays the "real window" pixels on the 
+ * tiny stick.
+ */
+void display_pixels( void )
+{
+  FastLED.show();
+}
+
+/* this function shows the eye, based on the head position, length, 
+ *  and direction
+ */
+void show_eye( void )
+{ 
+   int tail_pos;  // virtual window index of the tail
+
+   /* start with the virtual window filled with the background color */  
+   virtual_fill(background_color);
+
+   /* Draw in the eye, depending on direction */
+   if (current_eye_dir == DIR_RIGHT)
+   {
+     /* if we're going right, the tail is left of the eye...meaning we 
+      *  fillGradient from the tail to the head
+      */
+     tail_pos = eye_head_pos - eye_size;
+     fill_gradient_RGB(&(virtual_window[tail_pos]),
+                       eye_size,
+                       background_color,
+                       eye_color);
+
+     /* if the head of the eye is outside our real window, we want to make the
+      *  edge our eye color
+      */
+     if (eye_head_pos > REAL_WINDOW_END_INDEX)
+     {
+       virtual_window[REAL_WINDOW_END_INDEX] = eye_color;
+     }
+   }   // end of "moving right" adjustments
+   else
+   {
+      /* if we're going left, the tail is right of the eye, 
+       *  meaning we fill gradient from the head to tail.
+       */
+
+      // Note:  we don't really need this in this clause... 
+      tail_pos = eye_head_pos + eye_size;
+
+      fill_gradient_RGB(&(virtual_window[eye_head_pos]), 
+                        eye_size,
+                        eye_color,
+                        background_color);
+                        
+     /* if the head of the eye is outside our real window, we want to make the
+      *  edge our eye color
+      */
+     if (eye_head_pos < REAL_WINDOW_START_INDEX)
+     {
+       virtual_window[REAL_WINDOW_START_INDEX] = eye_color;
+     }    
+   }  // end of "moving left" adjustments
+
+   /* Now that we've updated our virtual window with the eye, do the 
+    *  actual display
+    */
+   display_pixels(); 
+}
+
+void update_display(void)
+{
+  static uint32_t last_update_time_ms = 0;
+  uint32_t        curr_time_ms;
+
+  curr_time_ms = millis();
+
+  /* if it hasn't been long enough for an update, just return.  */
+  if (last_update_time_ms + display_delay_ms > curr_time_ms)
+  {
+    return;
+  }
+
+  last_update_time_ms = curr_time_ms;
+
+  #ifdef PLATFORM_UNO
+  Serial.print("eye pos: ");
+  Serial.print(eye_head_pos);
+  Serial.print(", dir= ");
+  Serial.println(current_eye_dir);
+  #endif
+  
+  /* do we need to reverse direction? */
+  if ((current_eye_dir == DIR_RIGHT) && 
+      (eye_head_pos >= REAL_WINDOW_END_INDEX + eye_size))
+  {
+    current_eye_dir = DIR_LEFT;
+
+    /* because our head pos is far right in virtual window, we need to reset
+     *  it here the the right edge.
+     */
+    eye_head_pos = REAL_WINDOW_END_INDEX;
+  }
+  else if ((current_eye_dir == DIR_LEFT) &&
+           (eye_head_pos <= REAL_WINDOW_START_INDEX - eye_size))
+  {
+    current_eye_dir = DIR_RIGHT;
+
+    /* because our head pos is far left in virtual window, we need to reset
+     *  it here to the left edge.
+     */
+    eye_head_pos = REAL_WINDOW_START_INDEX;
+  }
+
+  /* move the eye one slot in the desired direction */
+  if (current_eye_dir == DIR_RIGHT)
+  {
+    eye_head_pos++;
+  }
+  else
+  {
+    eye_head_pos--;
+  }
+  
+  /* ...and display the eye on our pixels. */
+  show_eye();
+}
+
+
 void setup() 
 {
   int i;
@@ -155,5 +329,5 @@ void setup()
 
 void loop() 
 {  
- 
+  update_display();
 }
